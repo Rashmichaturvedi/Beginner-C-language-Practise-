0b6d26d95375def239d87a6d693dd82977184baa
Rashmi chaturvedi <rc1981999@gmail.com>
2020-03-11 23:48:06 +0530

    Update volwel&consonant.c

--- strings/volwel&consonant.c
+++ strings/volwel&consonant.c
@@ -5,7 +5,7 @@ int main()
 char ch;
  printf("enter the words");
  scanf("%c",&ch);
- if(ch=='a'|| ch=='e'|| ch=='i'||ch='o'||ch=='u'||ch=='A'|| ch=='E'|| ch=='I'||ch='O'||ch=='U')
+ if(ch=='a'|| ch=='e'|| ch=='i'||ch=='o'||ch=='u'||ch=='A'|| ch=='E'|| ch=='I'||ch=='O'||ch=='U')
  printf("\nVOLWEL  DETECTED %c",ch);
  else
  printf("\n %c is consonant ",ch);
