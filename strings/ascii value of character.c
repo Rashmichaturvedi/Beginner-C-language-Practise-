dcb7be2bb2a90be22648592a51b1c8cc8bc6cd43
Rashmi chaturvedi <rc1981999@gmail.com>
2020-03-14 23:42:24 +0530

    day 7 few last questions of string

---
+++ strings/ascii value of character.c
@@ -0,0 +1,16 @@
+//program to pring ascii value of all character
+
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    /* Print ASCII values from 0 to 255 */
+    for(i=0; i<=255; i++)
+    {
+        printf("ASCII value of character %c = %d\n", i, i);
+    }
+
+    return 0;
+    }
